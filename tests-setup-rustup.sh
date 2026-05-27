0fca6478646f63a15235b5be2c34e4198682038d
Peter Lyons Kehl <peter.kehl@gmail.com>
2026-05-27 14:08:37 -0700

    GitHub CI rustup setup..

--- tests-setup-rustup.sh
+++ tests-setup-rustup.sh
@@ -3,6 +3,5 @@
 # strict mode
 set -euo pipefail
 
-rustup component add rustfmt
 rustup install nightly --profile minimal
-rustup +nightly component add clippy miri
+rustup +nightly component add clippy rustfmt miri
